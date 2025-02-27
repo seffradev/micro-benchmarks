09482f60566d8ed1751648b6f02d5d1e19c5ab79
Hampus Avekvist <ha@leissner.se>
2025-02-27 18:59:41 +0100

    style: Clean up test

--- tests/EventHandler.cc
+++ tests/EventHandler.cc
@@ -4,21 +4,18 @@
 #include <memory>
 
 TEST(EventHandler, InheritanceAndVariantEqual) {
-    EXPECT_EQ(lambdas(5), inheritance(std::make_shared<IntEvent>(5)));
-    EXPECT_EQ(getIf(5), inheritance(std::make_shared<IntEvent>(5)));
-    EXPECT_EQ(holdsAlternative(5), inheritance(std::make_shared<IntEvent>(5)));
+    auto ie = std::make_shared<IntEvent>(5);
+    EXPECT_EQ(lambdas(5), inheritance(ie));
+    EXPECT_EQ(getIf(5), inheritance(ie));
+    EXPECT_EQ(holdsAlternative(5), inheritance(ie));
 
-    EXPECT_EQ(lambdas("foobar"),
-              inheritance(std::make_shared<StringEvent>("foobar")));
-    EXPECT_EQ(getIf("foobar"),
-              inheritance(std::make_shared<StringEvent>("foobar")));
-    EXPECT_EQ(holdsAlternative("foobar"),
-              inheritance(std::make_shared<StringEvent>("foobar")));
+    auto se = std::make_shared<StringEvent>("foobar");
+    EXPECT_EQ(lambdas("foobar"), inheritance(se));
+    EXPECT_EQ(getIf("foobar"), inheritance(se));
+    EXPECT_EQ(holdsAlternative("foobar"), inheritance(se));
 
-    EXPECT_EQ(lambdas(FatType{5, "foobar"}),
-              inheritance(std::make_shared<FatTypeEvent>(5, "foobar")));
-    EXPECT_EQ(getIf(FatType{5, "foobar"}),
-              inheritance(std::make_shared<FatTypeEvent>(5, "foobar")));
-    EXPECT_EQ(holdsAlternative(FatType{5, "foobar"}),
-              inheritance(std::make_shared<FatTypeEvent>(5, "foobar")));
+    auto fte = std::make_shared<FatTypeEvent>(5, "foobar");
+    EXPECT_EQ(lambdas(FatType{5, "foobar"}), inheritance(fte));
+    EXPECT_EQ(getIf(FatType{5, "foobar"}), inheritance(fte));
+    EXPECT_EQ(holdsAlternative(FatType{5, "foobar"}), inheritance(fte));
 }
